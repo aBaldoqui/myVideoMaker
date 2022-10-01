0eed853d5fe7d3ebbb2024157dc0621157a375bd
Arthur Baldoqui <arthur.baldoqui.bergamo@gmail.com>
2022-10-01 13:55:44 -0300

    perfeitinho

--- handlers/langunderstanding.js
+++ handlers/langunderstanding.js
@@ -2,16 +2,12 @@ const Language = require('@google-cloud/language')
 const state = require('./state')
 const language = new Language.LanguageServiceClient()
 
-const periods = {
-    speech: '',
-    keywords: [],
-    images: []
-}
 
 async function getKeyWords(arrayOfTxt) {
 
+    let arrayOfEntities = []
 
-    const entitiesArray = Promise.all(arrayOfTxt.map(async (element) => {
+    Promise.all(arrayOfTxt.map(async (element) => {
         const inputSpeech = element.alternatives[0].transcript
 
         const document = {
@@ -22,24 +18,52 @@ async function getKeyWords(arrayOfTxt) {
         return await language.analyzeEntities({ document: document })
 
 
-    }))
-    let index = 0
-    arrayOfTxt.forEach(element => {
-        const inputSpeech = element.alternatives[0].transcript
-        const result = entitiesArray[index]
+    })).then((resArray) => {
+        arrayOfEntities = resArray.map((item) => {
+            return item[0].entities
+        })
+        coontinuistaResolver(arrayOfEntities)
+    })
 
-        periods.speech = inputSpeech;
-        periods.images = [];
-        periods.meta = element.resultEndTime
-        const entities = result.entities;
-        periods.keywords = entities.map((ent) => { return ent.type });
-        index++
-        if(index >= arrayOfTxt.length-1){
-            state.save(periods)
-        }        
-    });
 
-    console.log(arrayOfTxt.length, (await entitiesArray).length)
+    function coontinuistaResolver(entitiesArray) {
+        let index = 0
+
+        const continuísta = arrayOfTxt.map((item) => {
+            const inputSpeech = item.alternatives[0].transcript
+            const result = entitiesArray[index]
+            index++
+
+            const foo = result.map((item) => {
+                return { name: item.name, type: item.type }
+            });
+
+            return {
+                speech: inputSpeech,
+                images: [],
+                meta: item.resultEndTime,
+                keywords:foo
+            }
+
+        })
+
+        state.save(continuísta)
+        // arrayOfTxt.forEach(element => {
+
+
+        //     index++
+        //     if (index > arrayOfTxt.length - 1) {
+        //         console.log('foi')
+        //         state.save(continuísta)
+        //     }
+        // });
+
+    }
+
+
+    // console.log(arrayOfTxt.length, (await entitiesArray).length)
+
+
 
     // .then(results => {
     //     const data = results.map((result) => {
